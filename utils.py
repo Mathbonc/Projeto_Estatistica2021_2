52f07d8dcc4ca624ebd5db4f1eb19bf370c97df5
Mathbonc <mjbo@cin.ufpe.br>
2022-05-21 08:16:50 -0300

    Final project

--- utils.py
+++
@@ -1,5 +0,0 @@
-def parseToNum(num_string):
-    if (num_string) and ('.' in num_string) and (not '?' in num_string):
-        return float(num_string)
-    elif (num_string) and (not '?' in num_string):
-        return int(num_string)
